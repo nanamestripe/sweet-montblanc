71671cfb8ac07b6a3a06ad3e8f8ee138b7ed1db3
Tottyan <20566363+nanamestripe@users.noreply.github.com>
2021-01-08 20:12:44 +0900

    delete default  and add new montblanc-child php

--- functions.php
+++ functions.php
@@ -1,28 +1,34 @@
 <?php
-// Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
 
-// BEGIN ENQUEUE PARENT ACTION
-// AUTO GENERATED - Do not modify or remove comment markers above or below:
+// function my_theme_enqueue_styles()
+// {
+//     $parent_style = 'parent-style'; // This is 'montblanc-style' for the montblanc theme.
 
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
-            $uri = get_template_directory_uri() . '/rtl.css';
-        return $uri;
-    }
-endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+//     wp_enqueue_style($parent_style, get_template_directory_uri() . '/css/woocommerce-layout.css');
+//     wp_enqueue_style(
+//         'child-style',
+//         get_template_directory_uri() . '/css/woocommerce-layout.css',
+//         array( $parent_style ),
+//         wp_get_theme()->get('Version')
+//     );
+// }
+// add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 
-if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-    function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
-    }
-endif;
-add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
 function theme_enqueue_scripts() {
-//子テーマのstyle.css
+//親テーマのstyle.css
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
+
+        //子テーマのstyle.css
         wp_enqueue_style('style-child', get_stylesheet_directory_uri() . '/style.css', array( 'style' ));
+
+//親テーマのstyle.css
+    wp_enqueue_style('woocommerce-layout', get_template_directory_uri() . '/css/woocommerce-layout.css');
+
+        //子テーマのstyle.css
+        wp_enqueue_style('woocommerce-layout-child', get_stylesheet_directory_uri() . '/css/woocommerce-layout.css', array( 'woocommerce-layout' ));
+
 
 }
 
@@ -36,13 +42,15 @@ function mybootstrap_enqueue_styles() {
 
 function mybootstrap_enqueue_scripts() {
     $dependencies = array('jquery');
-    wp_enqueue_script('bootstrap', get_stylesheet_directory_uri() . '/bootstrap/js/bootstrap.bundle.min.js', $dependencies, '3.5.1', true ); // trueとしてbodyタグのクロージングの前で読み込むように設定
+    wp_enqueue_script('bootstrap', get_stylesheet_directory_uri() . '/bootstrap/js/bootstrap.min.js', $dependencies, '3.4.1', true ); // trueとしてbodyタグのクロージングの前で読み込むように設定
 }
 
 add_action( 'wp_enqueue_scripts', 'mybootstrap_enqueue_styles' );
 add_action( 'wp_enqueue_scripts', 'mybootstrap_enqueue_scripts' );
 
-// END ENQUEUE PARENT ACTION
+
+
 
 /*【表示カスタマイズ】アイキャッチ画像の有効化 */
 add_theme_support('post-thumbnails');
+?>
