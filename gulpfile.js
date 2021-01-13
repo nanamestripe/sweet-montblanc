4c109b0a11140997f57e5f7c72f4bedba809bcfb
Tottyan <20566363+nanamestripe@users.noreply.github.com>
2021-01-13 16:46:07 +0900

    comile .less & .scss in parallel

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,57 @@
 // gulpプラグインの読み込み
 const gulp = require("gulp");
-// lessをコンパイルするプラグインの読み込み
+// Sassをコンパイルするプラグインの読み込み
+const sass = require("gulp-sass");
 const less = require("gulp-less");
 
-// *.lessをタスクを作成する
-gulp.task("default", function () {
-  // *.lessファイルを取得
-  return (
-    gulp
-      .src("css/*.less")
-      // lessのコンパイルを実行
-      .pipe(less())
-      // cssフォルダー以下に保存
-      .pipe(gulp.dest("css"))
-  );
+// style.scssの監視タスクを作成する
+gulp.task("sass", function () {
+  // ★ style.scssファイルを監視
+  return gulp.watch("style.scss", function () {
+    // style.scssの更新があった場合の処理
+
+    // style.scssファイルを取得
+    return (
+      gulp
+        .src("style.scss")
+        // Sassのコンパイルを実行
+        .pipe(
+          sass({
+            outputStyle: "expanded",
+          })
+            // Sassのコンパイルエラーを表示
+            // (これがないと自動的に止まってしまう)
+            .on("error", sass.logError)
+        )
+        // cssフォルダー以下に保存
+        .pipe(gulp.dest("."))
+    );
+  });
 });
+
+// *.lessの監視タスクを作成する
+gulp.task("less", function () {
+  // ★ *.lessファイルを監視
+  return gulp.watch("css/*.less", function () {
+    // *.lessの更新があった場合の処理
+
+    // *.lessファイルを取得
+    return (
+      gulp
+        .src("css/*.less")
+        // Sassのコンパイルを実行
+        .pipe(
+          less({
+            outputStyle: "expanded",
+          })
+            // lessのコンパイルエラーを表示
+            // (これがないと自動的に止まってしまう)
+            .on("error", less.logError)
+        )
+        // cssフォルダー以下に保存
+        .pipe(gulp.dest("css"))
+    );
+  });
+});
+
+gulp.task("default", gulp.parallel("sass", "less"));
