6ca9a66d9b193cfdcb37c013434d67b66fa0cb95
Tottyan <20566363+nanamestripe@users.noreply.github.com>
2021-01-15 11:56:36 +0900

    Replace "archive" with get_post_format()

--- archive.php
+++ archive.php
@@ -9,44 +9,45 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+    <?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
+    <header class="page-header">
+      <?php
 					// Show an optional term description.
 					$term_description = term_description();
 					if ( ! empty( $term_description ) ) :
 						printf( '<div class="taxonomy-description">%s</div>', $term_description );
 					endif;
 				?>
-			</header><!-- .page-header -->
+    </header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+    <?php /* Start the Loop */ ?>
+    <?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
+    <?php
 					/* Include the Post-Format-specific template for the content.
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
-					get_template_part( 'content', get_post_format() );
+					get_template_part( 'content','archive');
+					//get_post_format()を削除、'archive'を追記
 				?>
 
-			<?php endwhile; ?>
+    <?php endwhile; ?>
 
-			<?php montblanc_paging_nav(); ?>
+    <?php montblanc_paging_nav(); ?>
 
-		<?php else : ?>
+    <?php else : ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
+    <?php get_template_part( 'content', 'none' ); ?>
 
-		<?php endif; ?>
+    <?php endif; ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+  </main><!-- #main -->
+</section><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
